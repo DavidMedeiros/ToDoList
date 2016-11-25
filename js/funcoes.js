baf3483146dc17f8240d1f1702cbba19deef17d6
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2016-11-25 20:14:15 -0300

    tres itens pre definidos

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,9 +1,10 @@
 var i = 0;
 
-function processForm() {
+function processarForm() {
   i++;
   var item = document.minhaAgenda.itemDaAgenda.value;
-  var listado = '<div id="item' + i + '"><input type="checkbox" onclick="riscar(\'item'+ i + '\')" />' + item + '</div>';
+  var listado = '<div id="item' + i + '"><input type="checkbox" onclick="riscar(\'item'
+  + i + '\')" />' + item + '</div>';
   document.getElementById("agenda").innerHTML += listado;
 }
 
