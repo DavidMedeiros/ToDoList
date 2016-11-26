90a44ce3b380f9c3f2426b808b79ce17a34fb054
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2016-11-26 15:36:53 -0300

    uso de angular

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//app angular agenda - modulo da aplicacao
+angular.module('agenda',[]).
+controller('agendaController', ['$scope', function($scope){
+
+	//tarefas pré-definidas
+	$scope.tarefas = [
+		{'title': 'Marcar Dentista', 'done':false},
+		{'title': 'Terminar exercicios de metodos', 'done':false},
+		{'title': 'Ligar para Ana', 'done':false}
+	];
+
+	//metodo para adicionar uma nova tarefa
+	$scope.addItem = function(){
+		$scope.tarefas.push({'title':$scope.itemDaAgenda, 'done':false});
+		$scope.itemDaAgenda = '';
+	}
+
+	//metodo para remover uma nova tarefa
+	$scope.removeTarefa = function() {
+		$scope.tarefas.splice(this.$index, 1);
+	}
+
+	//metodo para exibir a quantidade de tarefas já concluidas
+	$scope.StatusTarefas = function () {
+		var count = 0;
+		angular.forEach($scope.tarefas, function (tarefa) {
+			count += tarefa.done ? 0 : 1;
+		});
+		return count;
+	};
+}]);
--- js/funcoes.js
+++ js/funcoes.js
@@ -1,13 +1,7 @@
-var i = 0;
-
-function processarForm() {
-  i++;
-  var item = document.minhaAgenda.itemDaAgenda.value;
-  var listado = '<div id="item' + i + '"><input type="checkbox" onclick="riscar(\'item'
-  + i + '\')" />' + item + '</div>';
-  document.getElementById("agenda").innerHTML += listado;
+function mudaCorTexto(div){
+	$(div).css({'color': 'red'});
 }
 
-function riscar(item) {
-	document.getElementById(item).className = "marcado";
+function restaurarCorTexto(div){
+	$(div).css({'color': 'black'});
 }
